fd2011cf2a08a49bfc2b25e3353d3d7b80cc06ce
OmarElnemr <omarahmedelnemr16@gmail.com>
2024-06-19 17:28:36 +0300

    use Query instead of Body

--- src/Server/Overview/OverviewController.ts
+++ src/Server/Overview/OverviewController.ts
@@ -5,25 +5,25 @@ import {Request,Response} from 'express';
 class OverviewController{
     // Get Overview on Finanials
     async getFinanials(req:Request,res:Response) {
-        const response = await overviewModel.GetFinanials(req.body)
+        const response = await overviewModel.GetFinanials(req.query)
         res.status(response['status']).json(response['data'])
     }
 
     // Get Overview on Assignments
     async getAssignments(req:Request,res:Response) {
-        const response = await overviewModel.GetAssignmentsOverview(req.body)
+        const response = await overviewModel.GetAssignmentsOverview(req.query)
         res.status(response['status']).json(response['data'])
     }
 
     // Get Overview on Statistics
     async getStatistics(req:Request,res:Response) {
-        const response = await overviewModel.GetStatisticsOverview(req.body)
+        const response = await overviewModel.GetStatisticsOverview(req.query)
         res.status(response['status']).json(response['data'])
     }
 
     // Get The Next Lecture Date
     async getNextLecture(req:Request,res:Response) {
-        const response = await overviewModel.GetNextLecture(req.body)
+        const response = await overviewModel.GetNextLecture(req.query)
         res.status(response['status']).json(response['data'])
     }
 
